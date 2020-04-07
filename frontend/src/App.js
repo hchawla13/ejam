68e6a72811e67ba7b555790fbb0383d819a61401
Harpreet Chawla <hchawla13@gmail.com>
2020-04-08 03:44:00 +0530

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,110 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactTable from 'react-table'
+import axios from 'axios';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+
+
+
 
 class App extends Component {
   state = {
     response: '',
     post: '',
     responseToPost: '',
+    initCity: 'Select',
+    weatherFinalData: '',
   };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+  getWeatherData = async () => {
+    if (this.state.initCity !== 'Select') {
+      const res = await this.callWeatherApi(this.state.initCity);
+      this.setState({ weatherFinalData: res })
+    }
   }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
+  callWeatherApi = async (cityName) => {
+    const response = await fetch(`/weatherData?city=${cityName}`);
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-
-    return body;
+    return body.data;
   };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
+  change = (event) => {
+    this.setState({ initCity: event.target.value });
+  }
   render() {
+    var data = {
+      "coord": {
+        "lon": -87.65,
+        "lat": 41.85
+      },
+      "weather": [
+        {
+          "id": 802,
+          "main": "Clouds",
+          "description": "scattered clouds",
+          "icon": "03d"
+        }
+      ],
+      "base": "stations",
+      "main": {
+        "temp": 298.86,
+        "feels_like": 294.58,
+        "temp_min": 298.15,
+        "temp_max": 299.82,
+        "pressure": 1002,
+        "humidity": 47
+      },
+      "visibility": 16093,
+      "wind": {
+        "speed": 7.7,
+        "deg": 230,
+        "gust": 11.8
+      },
+      "clouds": {
+        "all": 40
+      },
+      "dt": 1586294330,
+      "sys": {
+        "type": 1,
+        "id": 5228,
+        "country": "US",
+        "sunrise": 1586258533,
+        "sunset": 1586305348
+      },
+      "timezone": -18000,
+      "id": 4887398,
+      "name": "Chicago",
+      "cod": 200
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <label htmlFor="city">Choose a city:</label>
+        <select id="city" onChange={this.change} value={this.state.initCity}>
+          <option value="Select">Select</option>
+          <option value="chicago">Chicago</option>
+          <option value="houston">Houston</option>
+          <option value="dallas">Dallas</option>
+        </select>
+        <button onClick={this.getWeatherData}>Send</button>
+        <p>Selected city is <span>{this.state.initCity}</span></p>
+        {/* <p>{JSON.stringify(this.state.weatherFinalData)}</p> */}
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Latitude</Th>
+              <Th>Longitude</Th>
+              <Th>Temperature</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>{this.state.weatherFinalData ? this.state.weatherFinalData.coord.lat : 'Default latitude'}</Td>
+              <Td>{this.state.weatherFinalData ? this.state.weatherFinalData.coord.lon : 'Default latitude'}</Td>
+              <Td>{this.state.weatherFinalData ? this.state.weatherFinalData.main.temp : 'Default temperature'}</Td>
+            </Tr>
+          </Tbody>
+        </Table>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fetch = require('node-fetch')
+const axios = require('axios')
+var Request = require("request")
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+const cityName = 'dallas';
+
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather'
+const APPID = '552e3217e21af17ca9062f1016cd3d85'
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express world' });
+    res.send({ express: 'Hello From Express world ' });
+});
+
+app.get('/weatherData', (req, res) => {
+    const fullUrl = `${baseUrl}?q=${req.query.city}&appid=${APPID}`;
+    Request.get(fullUrl, (error, response, body) => {
+        if (error) {
+            return console.dir(error);
+        }
+        res.send({ data: JSON.parse(body) });
+    });
+
+
 });
 
 app.post('/api/world', (req, res) => {
-    console.log(req.body);
     res.send(
         `I received your POST request. This is what you sent me: ${req.body.post}`,
     );
